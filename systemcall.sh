06543d782f789120a1492f9c3125dbbeebcdee59
Abdullah Ibne Hanif Arean <70502574+AbdullahArean@users.noreply.github.com>
2022-09-27 08:32:50 +0600

    undefine reference to table only, bug is tried to be fixed

--- systemcall.sh
+++ systemcall.sh
@@ -4,8 +4,7 @@
 # Email  : abdullaharean2613@gmail.com
 #Variable
 linux_version = $1;
-s_name = $2;
-
+sys_call_name= $2;
 # Building Environment
 sudo apt install git
 sudo apt install make
@@ -21,32 +20,32 @@ sudo apt install libelf-dev
 sudo apt install bison
 sudo apt install libncurses-dev
 # Download & Extract Kernel Source
-wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$1.tar.xz;
+#wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$1.tar.xz;
 tar xvf linux-$1.tar.xz;
-cat systemcallcode.c >> $2.c
+cat systemcallcode.c >> ${sys_call_name}.c
 cd linux-$1;
 # read and write from a c file
-mkdir $2
+mkdir ${sys_call_name}
 cd ..
-mv $2.c linux-$1/$2/
-cd linux-$1/$2/
+mv ${sys_call_name}.c linux-$1/${sys_call_name}/
+cd linux-$1/${sys_call_name}/
 touch Makefile
-printf "obj-y:= $2.o" > Makefile
+printf "obj-y:= ${sys_call_name}.o" > Makefile
 cd ..
 #edit necessary things for me
-sed -i '/^core-y +=/ s/$/ sys_call1\//' Makefile
-sed -i "\$i asmlinkage long sys_sys_call1(void) ;" include/linux/syscalls.h
-sed -i '/^334.*/a 335 common  sys_call1       sys_sys_call1' arch/x86/entry/syscalls/syscall_64.tbl
+sed -i "/^core-y +=/ s/$/ ${sys_call_name}\//" Makefile
+sed -i "\$i asmlinkage long sys_${sys_call_name}(void) ;" include/linux/syscalls.h
+sed -i "/^334.*/a 335 common  ${sys_call_name}       sys_${sys_call_name}" arch/x86/entry/syscalls/syscall_64.tbl
 # Compile The Kernel
 make clean
 make mrproper
-wget https://raw.github.com/xanmod/linux/5.17/CONFIGS/xanmod/gcc/config_x86-64
+wget https://raw.github.com/xanmod/linux/${linux_version:0:3}/CONFIGS/xanmod/gcc/config_x86-64
 mv config_x86-64 .config
 scripts/config --disable SYSTEM_REVOCATION_KEYS
 scripts/config --disable SYSTEM_TRUSTED_KEYS
 scripts/config --disable CONFIG_X86_X32
 yes "" | make oldconfig
-make -j$(nproc)
-sudo make modules_install -j$(nproc)
-sudo make install -j$(nproc)
+# make -j$(nproc)
+# sudo make modules_install -j$(nproc)
+# sudo make install -j$(nproc)
 # reboot
