8aba2e623f765979f419a040e55e8574c37da585
Abdullah Ibne Hanif Arean <70502574+AbdullahArean@users.noreply.github.com>
2022-09-27 09:47:16 +0600

    stable version of systemcall automation

--- systemcall.sh
+++ systemcall.sh
@@ -4,7 +4,8 @@
 # Email  : abdullaharean2613@gmail.com
 #Variable
 linux_version = $1;
-sys_call_name= $2;
+s_name = $2;
+
 # Building Environment
 sudo apt install git
 sudo apt install make
@@ -22,20 +23,25 @@ sudo apt install libncurses-dev
 # Download & Extract Kernel Source
 #wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$1.tar.xz;
 tar xvf linux-$1.tar.xz;
-cat systemcallcode.c >> ${sys_call_name}.c
+cat systemcallcode.c >> $2.c
 cd linux-$1;
 # read and write from a c file
-mkdir ${sys_call_name}
+mkdir $2
 cd ..
-mv ${sys_call_name}.c linux-$1/${sys_call_name}/
-cd linux-$1/${sys_call_name}/
+mv $2.c linux-$1/$2/
+cd linux-$1/$2/
 touch Makefile
-printf "obj-y:= ${sys_call_name}.o" > Makefile
+printf "obj-y:= $2.o" > Makefile
 cd ..
 #edit necessary things for me
-sed -i "/^core-y +=/ s/$/ ${sys_call_name}\//" Makefile
-sed -i "\$i asmlinkage long sys_${sys_call_name}(void) ;" include/linux/syscalls.h
-sed -i "/^334.*/a 335 common  ${sys_call_name}       sys_${sys_call_name}" arch/x86/entry/syscalls/syscall_64.tbl
+# sed -i /^core-y +=/ s/$/ $2\//" Makefile
+# sed -i "\$i asmlinkage long sys_$2(void) ;" include/linux/syscalls.h
+# sed -i "/^334.*/a 335 common  $2        sys_$2" arch/x86/entry/syscalls/syscall_64.tbl
+
+sed -i '/^core-y +=/ s/$/ sys_call1\//' Makefile
+#-----above line not working
+sed -i "\$i asmlinkage long sys_sys_call1(void) ;" include/linux/syscalls.h
+sed -i '/^334.*/a 335 common  sys_call1     sys_sys_call1' arch/x86/entry/syscalls/syscall_64.tbl
 # Compile The Kernel
 make clean
 make mrproper
@@ -45,7 +51,7 @@ scripts/config --disable SYSTEM_REVOCATION_KEYS
 scripts/config --disable SYSTEM_TRUSTED_KEYS
 scripts/config --disable CONFIG_X86_X32
 yes "" | make oldconfig
-# make -j$(nproc)
-# sudo make modules_install -j$(nproc)
-# sudo make install -j$(nproc)
+make -j$(nproc)
+sudo make modules_install -j$(nproc)
+sudo make install -j$(nproc)
 # reboot
